7a4ee51a38af281fc00548c1f180af41d1ce535c
0murbekovv <ilyasgs132@gmail.com>
2021-03-15 16:56:09 +0600

    reddit

---
+++ main/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from posts.views import *
+
+
+router = DefaultRouter()
+router.register('posts', PostsViewSet)
+router.register('comment', CommentViewSet)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('v1/api/categories/', CategoryListView.as_view()),
+    path('v1/api/add-image/', PostImageView.as_view()),
+    path('v1/api/account/', include('user.urls')),
+    path('v1/api/', include(router.urls)),
+    path('v1/api/posts/<int:pk>/like/', LikeView.as_view()),
+    path('v1/api/posts/<int:pk>/favorite/', FavoriteView.as_view())
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ posts/views.py
@@ -0,0 +1,129 @@
+from datetime import timedelta
+
+from django.db.models import Q
+from django.shortcuts import render
+from django.utils import timezone
+from rest_framework.decorators import api_view, action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
+from rest_framework import generics, viewsets, status, mixins
+from rest_framework.viewsets import ModelViewSet
+
+from .permissions import IsPostAuthor
+
+from .models import *
+from .serializers import *
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny, ]
+
+
+class PostsViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permissions = [IsPostAuthor, ]
+        else:
+            permissions = [IsAuthenticated, ]
+        return [permission() for permission in permissions]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        days_count = int(self.request.query_params.get('days', 0))
+        if days_count > 0:
+            start_date = timezone.now() - timedelta(days=days_count)
+            queryset = queryset.filter(created_at__gte=start_date)
+        return queryset
+
+    @action(detail=False, methods=['get'])
+    def own(self, request, pk=None):
+        queryset = self.get_queryset()
+        queryset = queryset.filter(author=request.user)
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, pk=None):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) |
+                                   Q(text__icontains=q))
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class PostImageView(generics.ListCreateAPIView):
+    queryset = PostImage.objects.all()
+    serializer_class = PostImageSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+class CommentViewSet(ModelViewSet, mixins.DestroyModelMixin):
+    queryset = Comments.objects.all()
+    serializer_class = CommentSerializer
+
+    class Meta:
+        model = Comments
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('Вы не комментировали')
+
+
+class LikeView(generics.CreateAPIView, mixins.DestroyModelMixin):
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Like.objects.filter(liker=user, post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('Вы уже поставили Лайк :)')
+        serializer.save(liker=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('Вы не ставили Лайк')
+
+
+class FavoriteView(generics.CreateAPIView, mixins.DestroyModelMixin):
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Favorite.objects.filter(user=user, post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('Вы уже добавили в избранные.')
+        serializer.save(user=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('вы не добавляли в избранные.')
